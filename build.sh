8adc35eaefd94758b72ee0f6fcc875f978e83e6c
Harsh Jain <81467097+harsh70568@users.noreply.github.com>
2024-11-30 01:29:13 +0530

    Update build.sh

--- build.sh
+++ build.sh
@@ -1,5 +1,6 @@
 pip install -r requirements.txt
 #python manage.py makemigrations frontend
 #python manage.py migrate
+python -m pip install --upgrade pip
 python manage.py runserver 8000
 python -m uvicorn backend.api.main:app --reload 7000
