84272d690ff24309cd189bb7d84aeb63f3f51c61
harsh.backend <harsh.2024it1034@kiet.edu>
2024-11-30 01:21:02 +0530

    Added makemigrations frontend

--- build.sh
+++ build.sh
@@ -1,4 +1,5 @@
 pip install -r requirements.txt
+python manage.py makemigrations frontend
 python manage.py migrate
 python manage.py runserver 8000
 python -m uvicorn backend.api.main:app --reload 7000
