9e6cb7cf29d4fd79838079e3c9a73e1d5a80d104
harsh.backend <harsh.2024it1034@kiet.edu>
2024-12-01 12:47:36 +0530

    Fixed build.sh

--- build.sh
+++ build.sh
@@ -1,5 +1,7 @@
 pip install -r requirements.txt
-python manage.py collectstatic
+python manage.py collectstatic --noinput
 python -m pip install --upgrade pip
+echo "Starting Django server on port 8000..."
+python manage.py runserver 0.0.0.0:8000 & 
 echo "Starting FastAPI server on port 7000..."
 python -m uvicorn backend.api.main:app --host 0.0.0.0 --port 7000
