76406b47fa99c485b1acdf9e8467fd3f7df97ea7
harsh.backend <harsh.2024it1034@kiet.edu>
2024-12-01 12:09:58 +0530

    Fixed style.css not loading

--- CodeBazar/settings.py
+++ CodeBazar/settings.py
@@ -24,7 +24,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-mlsvx&-x0_ny&o64t0qq1rb$hqylrdm6gp#5)u0^z1mvv!!tk!'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
 ALLOWED_HOSTS = ['localhost', '127.0.0.1', 'codebazar.onrender.com']
 
